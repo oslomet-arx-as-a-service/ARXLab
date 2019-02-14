e017f214e54d33af890ae863085ddcced5ad122a
JeremiahUy <jery_3000@hotmail.com>
2019-02-14 11:59:33 +0100

    ADD analysis function for result

--- src/main/java/jeremiah/Main.java
+++ src/main/java/jeremiah/Main.java
@@ -1,17 +1,17 @@
 package jeremiah;
 
-import org.apache.poi.ss.formula.functions.T;
 import org.deidentifier.arx.*;
 import org.deidentifier.arx.Data.DefaultData;
 import org.deidentifier.arx.aggregates.StatisticsEquivalenceClasses;
 import org.deidentifier.arx.criteria.KAnonymity;
 import org.deidentifier.arx.ARXResult;
 import org.deidentifier.arx.metric.InformationLoss;
-import org.deidentifier.arx.risk.RiskModelAttributes;
+import org.deidentifier.arx.risk.RiskModelPopulationUniqueness;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Set;
 
 public class Main {
     static DefaultData data = Data.create();
@@ -80,59 +80,102 @@ public class Main {
         anonymizer.setHistorySize(200);
     }
 
-    public static void NodeResult(ARXResult result,Data dataAll){
-        ARXLattice.ARXNode node = result.getGlobalOptimum();
-        String test = Arrays.toString(node.getTransformation());
-        System.out.println("- Solution ");
-        System.out.println(test);
+    //.......................................ANALYSIS METHODS-----------------------------------------------------//
 
-        ARXPopulationModel pModel =
-                ARXPopulationModel.create(dataAll.getHandle().getNumRows(),
-                        0.01d);
 
-        double riskP = result.getOutput()
+    public static Double getLowestProsecutorRisk(ARXResult result, ARXPopulationModel pModel){
+        return result.getOutput()
                 .getRiskEstimator(pModel)
                 .getSampleBasedReidentificationRisk()
-                .getEstimatedProsecutorRisk();
+                .getLowestRisk();
+    }
 
-        double riskJ = result.getOutput()
+    public static Double getRecodsAffectByRisk(ARXResult result, ARXPopulationModel pModel,double risk){
+        return result.getOutput()
                 .getRiskEstimator(pModel)
-                .getSampleBasedReidentificationRisk()
-                .getEstimatedJournalistRisk();
-
-        double riskM = result.getOutput()
-                .getRiskEstimator(pModel)
-                .getSampleBasedReidentificationRisk()
-                .getEstimatedMarketerRisk();
-
-        System.out.println("  * Prosecutor re-identification risk: " + riskP);
-        System.out.println("  * Journalist re-identification risk: " + riskJ);
-        System.out.println("  * Marketer   re-identification risk: " + riskM);
-
-        InformationLoss<?> vo = result.getGlobalOptimum().getLowestScore();
-        InformationLoss<?> v1 = result.getGlobalOptimum().getHighestScore();
-        System.out.println("- Information Loss " );
-        System.out.println(vo+"/"+v1);
-
-
-        StatisticsEquivalenceClasses v2 = result.getOutput(result.getGlobalOptimum(), false).getStatistics().getEquivalenceClassStatistics();
-        System.out.println("- Statistics");
-        System.out.println(v2);
-
-        String v3 = "";
-        v3 = v3 + dataAll.getHandle().getView().getNumRows();
-        v3 = v3 + " records with ";
-        v3 = v3 + dataAll.getDefinition().getQuasiIdentifyingAttributes().size();
-        v3 = v3 + " quasi-identifiers";
-        System.out.println("- Data: ");
-        System.out.println(v3);
-
-        String v4 = String.valueOf(result.getLattice().getSize());
-        System.out.println("- Policies available ");
-        System.out.println(v4);
+                .getSampleBasedRiskDistribution()
+                .getFractionOfRecordsAtRisk(risk);
 
     }
 
+    public static Double getAverageProsecutorRisk(ARXResult result, ARXPopulationModel pModel){
+        return result.getOutput()
+                .getRiskEstimator(pModel)
+                .getSampleBasedReidentificationRisk()
+                .getAverageRisk();
+    }
+
+    public static Double getHighestProsecutorRisk(ARXResult result, ARXPopulationModel pModel){
+        return result.getOutput()
+                .getRiskEstimator(pModel)
+                .getSampleBasedReidentificationRisk()
+                .getHighestRisk();
+    }
+
+    public static Double getEstimatedProsecutorRisk(ARXResult result, ARXPopulationModel pModel){
+        return result.getOutput()
+                .getRiskEstimator(pModel)
+                .getSampleBasedReidentificationRisk()
+                .getEstimatedProsecutorRisk();
+    }
+
+    public static Double getEstimatedJournalistRisk(ARXResult result, ARXPopulationModel pModel){
+        return result.getOutput()
+                .getRiskEstimator(pModel)
+                .getSampleBasedReidentificationRisk()
+                .getEstimatedJournalistRisk();
+    }
+
+    public static Double getEstimatedMarketerRisk(ARXResult result, ARXPopulationModel pModel){
+        return result.getOutput()
+                .getRiskEstimator(pModel)
+                .getSampleBasedReidentificationRisk()
+                .getEstimatedMarketerRisk();
+    }
+
+    public static Double getSampleUniques(ARXResult result, ARXPopulationModel pModel){
+        return result.getOutput()
+                .getRiskEstimator(pModel)
+                .getSampleBasedUniquenessRisk()
+                .getFractionOfUniqueTuples();
+    }
+
+    public static Double getPopulationUniques(ARXResult result, ARXPopulationModel pModel){
+        return result.getOutput()
+                .getRiskEstimator(pModel)
+                .getPopulationBasedUniquenessRisk()
+                .getFractionOfUniqueTuples(getPopulationModel(result,pModel));
+    }
+
+    public static RiskModelPopulationUniqueness.PopulationUniquenessModel getPopulationModel(ARXResult result, ARXPopulationModel pModel){
+        return
+                result.getOutput()
+                .getRiskEstimator(pModel)
+                .getPopulationBasedUniquenessRisk()
+                .getPopulationUniquenessModel();
+    }
+
+    public static Set<String> getQuasiIdentifiers(ARXResult result){
+        return result.getDataDefinition().getQuasiIdentifyingAttributes();
+    }
+
+    public static void showAnalysisData(ARXResult result,ARXPopulationModel pModel){
+        System.out.println("Measure: Value [%]");
+        System.out.println("Lowest risk: "+getLowestProsecutorRisk(result,pModel)*100);
+        System.out.println("Records affected by lowest risk: " + getRecodsAffectByRisk(result,pModel,getLowestProsecutorRisk(result,pModel))*100);
+        System.out.println("Average prosecutor risk: " + getAverageProsecutorRisk(result,pModel)*100);
+        System.out.println("Highest prosecutor risk: " + getHighestProsecutorRisk(result,pModel)*100);
+        System.out.println("Record affected by highest risk: " + getRecodsAffectByRisk(result,pModel,getHighestProsecutorRisk(result,pModel))*100);
+        System.out.println("Estimated prosecutor risk: " + getEstimatedProsecutorRisk(result,pModel)*100);
+        System.out.println("Estimated journalist risk: " + getEstimatedJournalistRisk(result,pModel)*100);
+        System.out.println("Estimated marketer risk: " + getEstimatedMarketerRisk(result,pModel)*100);
+        System.out.println("Sample uniques: " + getSampleUniques(result,pModel)*100);
+        System.out.println("Population uniques: " + getPopulationUniques(result,pModel)*100);
+        System.out.println("Population model: " + getPopulationModel(result,pModel));
+        System.out.println("Quasi-identifiers: " + getQuasiIdentifiers(result));
+    }
+
+    //---------------------------------------------------------------------------------------------------------------//
 
     public static void main(String[] args) throws IOException {
 
@@ -144,7 +187,13 @@ public class Main {
        File newfile = new File("C:/Users/jeuy/Desktop/bachelor/test.txt");
        result = anonymizer.anonymize(data,config);
 
-       NodeResult(result,data);
+        ARXPopulationModel pModel = ARXPopulationModel.create(data.getHandle().getNumRows(), 0.01d);
+
+        //-------ANALYSIS METHOD CALLS ---------------------------------------------------------///
+        showAnalysisData(result,pModel);
+
+       //NodeResult(result,data,pModel);
+        //-------------------------------------------------------------------------------------///
 
        //setter resultatet lik et datahandle objelkt for å kunne hente statisktikk bak anonymiliseringen
        handle = result.getOutput();
